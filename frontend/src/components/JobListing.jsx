df676a82f6cfad35a52f5988b9426be5f1e56c78
Zakari Adamu <53929284+ZakariAdamu@users.noreply.github.com>
2024-08-20 00:39:08 +0100

    Update JobListing.jsx

--- frontend/src/components/JobListing.jsx
+++ frontend/src/components/JobListing.jsx
@@ -41,7 +41,7 @@ function JobListing({ job }) {
 						{job.location}
 					</div>
 					<Link
-						to={`/jobs/${job._id}`}
+						to={`/${job._id}`}
 						className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
 					>
 						Read More
