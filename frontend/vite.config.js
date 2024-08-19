8bf693e7239f961ddaf326f7990160823568faa1
Zakari Adamu <53929284+ZakariAdamu@users.noreply.github.com>
2024-08-19 23:56:49 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
 	server: {
 		port: 3000,
 		proxy: {
-			"/api": "https://react-jobs-ruddy.vercel.app",
+			"/api": "https://react-jobs-ruddy.vercel.app/api",
 		},
 	},
 });
